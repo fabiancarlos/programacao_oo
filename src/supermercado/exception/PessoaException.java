5bcefe241bd47d359695f3f9dc264c8661610746
Fabian Carlos <fabian.pow@gmail.com>
2013-02-20 10:29:31 -0400

    class PessoaException

---
+++ src/supermercado/exception/PessoaException.java
@@ -0,0 +1,11 @@
+package supermercado.exception;
+
+public class PessoaException extends Exception {
+	
+	public static final String NOME_INVALIDO = "Nome deve possuir entre 6 e 60 caracteres";
+	
+	public PessoaException(String msg){
+		super(msg);
+	}
+
+}
