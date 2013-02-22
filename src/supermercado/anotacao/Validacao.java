49a1ce4340c102bd0327bcc039584d65dfc18b39
Fabian Carlos <fabian.pow@gmail.com>
2013-02-22 11:41:35 -0400

    interface validacao

---
+++ src/supermercado/anotacao/Validacao.java
@@ -0,0 +1,13 @@
+package supermercado.anotacao;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Validacao {
+    boolean requerido () default false;
+
+}
